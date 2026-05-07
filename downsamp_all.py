13746a7851e6c7bdfe2ee3d55080ccc7f550fb2a
Taechatam <taechatam.mal@gmail.com>
2026-05-07 15:02:43 +0700

    Update downsamp_all.py

--- downsamp_all.py
+++ downsamp_all.py
@@ -51,6 +51,6 @@ else:
 final_df = final_df.drop(columns=['batch', 'group'])
 
 # Save to new CSV
-final_df.to_csv(r"Github_test\output1_downsamp\test_balanced_data_annotate_cell.csv", index=False)
+final_df.to_csv(r"Github_test\output2_downsamp\test_balanced_data_annotate_cell.csv", index=False)
 
 print("Balanced dataset saved!")
